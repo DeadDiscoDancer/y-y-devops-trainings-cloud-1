d7b73e6e8559c7f473f71823b47b7014c6e04a08
Pavel Borisov <42678233+DeadDiscoDancer@users.noreply.github.com>
2023-11-09 20:36:02 +0300

    Add files via upload

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,11 +13,11 @@ provider "yandex" {
   zone                     = "ru-central1-a"
 }
 
-resource "yandex_vpc_network" "foo" {}
+resource "yandex_vpc_network" "ynet" {}
 
-resource "yandex_vpc_subnet" "foo" {
+resource "yandex_vpc_subnet" "ysubnet" {
   zone           = "ru-central1-a"
-  network_id     = yandex_vpc_network.foo.id
+  network_id     = yandex_vpc_network.ynet.id
   v4_cidr_blocks = ["10.5.0.0/24"]
 }
 
@@ -26,9 +26,9 @@ resource "yandex_container_registry" "registry1" {
 }
 
 locals {
-  folder_id = "<INSERT YOUR FOLDER ID>"
+  folder_id = "b1g3f87cb3s6b33dg6jl"
   service-accounts = toset([
-    "catgpt-sa",
+    "hewki-sa-2",
   ])
   catgpt-sa-roles = toset([
     "container-registry.images.puller",
@@ -42,7 +42,7 @@ resource "yandex_iam_service_account" "service-accounts" {
 resource "yandex_resourcemanager_folder_iam_member" "catgpt-roles" {
   for_each  = local.catgpt-sa-roles
   folder_id = local.folder_id
-  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["catgpt-sa"].id}"
+  member    = "serviceAccount:${yandex_iam_service_account.service-accounts["hewki-sa-2"].id}"
   role      = each.key
 }
 
@@ -50,31 +50,109 @@ data "yandex_compute_image" "coi" {
   family = "container-optimized-image"
 }
 resource "yandex_compute_instance" "catgpt-1" {
-    platform_id        = "standard-v2"
-    service_account_id = yandex_iam_service_account.service-accounts["catgpt-sa"].id
-    resources {
-      cores         = 2
-      memory        = 1
-      core_fraction = 5
+  platform_id        = "standard-v2"
+  service_account_id = yandex_iam_service_account.service-accounts["hewki-sa-2"].id
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 5
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.ysubnet.id
+    nat       = true
+  }
+  boot_disk {
+    initialize_params {
+      type     = "network-hdd"
+      size     = "30"
+      image_id = data.yandex_compute_image.coi.id
     }
-    scheduling_policy {
-      preemptible = true
-    }
-    network_interface {
-      subnet_id = "${yandex_vpc_subnet.foo.id}"
-      nat = true
-    }
-    boot_disk {
-      initialize_params {
-        type = "network-hdd"
-        size = "30"
-        image_id = data.yandex_compute_image.coi.id
-      }
-    }
-    metadata = {
-      docker-compose = file("${path.module}/docker-compose.yaml")
-      ssh-keys  = "ubuntu:${file("~/.ssh/devops_training.pub")}"
-    }
+  }
+  metadata = {
+	serial-port-enable = 1
+	user-data = "#cloud-config\nruncmd:\n  - wget -O - https://monitoring.api.cloud.yandex.net/monitoring/v2/unifiedAgent/config/install.sh | bash"
+    docker-compose = file("${path.module}/docker-compose.yaml")
+#    ssh-keys       = "ubuntu:${file("~/.ssh/devops_training.pub")}"
+    ssh-keys = file("${path.module}/meta.txt")
+  }
 }
 
+resource "yandex_compute_instance" "catgpt-2" {
+  platform_id        = "standard-v2"
+  service_account_id = yandex_iam_service_account.service-accounts["hewki-sa-2"].id
+  resources {
+    cores         = 2
+    memory        = 1
+    core_fraction = 5
+  }
+  scheduling_policy {
+    preemptible = true
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.ysubnet.id
+    nat       = true
+  }
+  boot_disk {
+    initialize_params {
+      type     = "network-hdd"
+      size     = "30"
+      image_id = data.yandex_compute_image.coi.id
+    }
+  }
+  metadata = {
+	serial-port-enable = 1
+	user-data = "#cloud-config\nruncmd:\n  - wget -O - https://monitoring.api.cloud.yandex.net/monitoring/v2/unifiedAgent/config/install.sh | bash"
+    docker-compose = file("${path.module}/docker-compose.yaml")
+    ssh-keys       = "ubuntu:${file("~/.ssh/devops_training.pub")}"
+  }
+}
 
+resource "yandex_lb_target_group" "lb_group" {
+  name      = "my-target-group"
+#  region_id = "${yandex_vpc_subnet.ysubnet.zone}"
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.ysubnet.id}"
+    address   = "${yandex_compute_instance.catgpt-1.network_interface.0.ip_address}"
+  }
+
+  target {
+    subnet_id = "${yandex_vpc_subnet.ysubnet.id}"
+    address   = "${yandex_compute_instance.catgpt-2.network_interface.0.ip_address}"
+  }
+}
+
+resource "yandex_lb_network_load_balancer" "ylb" {
+  name = "ylb"
+
+  listener {
+    name = "my-listener"
+    port = 8080
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  listener {
+    name = "metrics"
+    port = 9090
+    external_address_spec {
+      ip_version = "ipv4"
+    }
+  }
+
+  attached_target_group {
+    target_group_id = "${yandex_lb_target_group.lb_group.id}"
+
+    healthcheck {
+      name = "http"
+      http_options {
+        port = 8080
+        path = "/ping"
+      }
+    }
+  }
+}
